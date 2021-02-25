661f5c8b702d7d7644863348a5672fd67ebd20fa
Preethi Prabhakar <pprabhakar27@dxc.com>
2021-02-25 23:06:59 +0530

    Support to JIRA integration

---
+++ UI/src/scripts/collector.jsx
@@ -0,0 +1,12 @@
+// function collector_support(){
+//    window.ATL_JQ_PAGE_PROPS =  {
+// 	"triggerFunction": function(showCollectorDialog) {
+// 		//Requires that jQuery is available! 
+// 		jQuery("#myCustomTrigger").click(function(e) {
+// 			e.preventDefault();
+// 			showCollectorDialog();
+// 		});
+// 	}};
+// }
+
+// export default collector_support;
